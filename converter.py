dadc1669a28b4fc6686d200ab4ad0c4e952ecf3d
AiI-Iades <aschaud000@gmail.com>
2026-07-04 15:22:56 -0400

    add converter.py

---
+++ converter.py
@@ -0,0 +1,6 @@
+def convert(temp, from_unit, to_unit):
+    # Conversion logic using standard formulas
+    if from_unit == to_unit:
+        return temp
+    # Implement all unit conversions here
+    pass
